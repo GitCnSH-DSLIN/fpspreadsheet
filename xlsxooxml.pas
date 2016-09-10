b461eda7d15434c2fb5842ea50f57bc43d31f744
Christian U <info@cu-tec.de>
2016-09-10 21:01:53 +0200

    Fix compiling with fpc 2.6.4

--- xlsxooxml.pas
+++ xlsxooxml.pas
@@ -2922,7 +2922,7 @@ begin
       SplitHyperlink(hyperlink^.Target, target, bookmark);
       if target <> '' then
       begin
-        if (pos('file:', target) = 0) and FileNameIsAbsolute(target) then
+        if (pos('file:', target) = 0) {$IF (fpc_version >= 3)} and FileNameIsAbsolute(target){$ENDIF} then
           FileNameToURI(target);
 //          target := 'file:///' + target;
         s := Format('Id="rId%d" Type="%s" Target="%s" TargetMode="External"',
